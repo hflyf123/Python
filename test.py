1b4c46f2a935e6cf825754d06eb6ed054ed820e4
lyf <hflyf123@qq.com>
2018-05-04 09:30:14 +0800

    20180504
    20180504

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-python -c "import sys; print sys.getdefaultencoding()"
+# Link:https://github.com/hflyf123
+# @Author: LYF
+# @Date: 2018-04-24 07:59:28
+# @Last Modified by:   LYF
+# @Last Modified time: 2018-04-24 07:59:28
+
+
+print("希望这一周也是顺利的一周")
--- 二分搜索.py
+++ 二分搜索.py
@@ -1,17 +1,23 @@
+# Link:https://github.com/hflyf123
+# @Author: LYF
+# @Date: 2018-04-23 21:49:34
+# @Last Modified by:   LYF
+# @Last Modified time: 2018-04-23 21:49:34
+
+
 def binarySearch(arr, x):
     low = 0
     high = len(arr) - 1
     while low <= high:
         mid = (low + high) // 2
         if x == arr[mid]:
-        	return mid
+            return mid
         elif x > arr[mid]:
-        	low = mid+1
+            low = mid+1
         elif x < arr[mid]:
-        	high = mid-1
+            high = mid-1
         else:
-        	return None
+            return None
 
-print(binarySearch([1,2,3],3))
-print(binarySearch([1,2,3],4))
-
+print(binarySearch([1, 2, 3], 3))
+print(binarySearch([1, 2, 3], 4))
